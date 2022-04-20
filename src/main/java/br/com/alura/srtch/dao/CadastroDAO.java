57b0d3c3974248bc8bee2af236f4aa7e9b707d6c
g-andradd <gabrielandraad@gmail.com>
2022-04-20 09:25:13 -0300

    crud CadastroDAO feito

--- src/main/java/br/com/alura/srtch/dao/CadastroDAO.java
+++ src/main/java/br/com/alura/srtch/dao/CadastroDAO.java
@@ -1,8 +1,9 @@
 package br.com.alura.srtch.dao;
 
-import br.com.alura.srtch.modelo.Cliente;
+import br.com.alura.srtch.modelo.Cadastro;
 
 import javax.persistence.EntityManager;
+import java.util.List;
 
 public class CadastroDAO {
 
@@ -12,8 +13,21 @@ public class CadastroDAO {
         this.em = em;
     }
 
-    public void cadastrar(Cliente cliente){
-        this.em.persist(cliente);
+    public List<Cadastro> buscarTodos() {
+        String jpql = "SELECT c FROM Cadastro c";
+        return em.createQuery(jpql, Cadastro.class).getResultList();
+    }
+
+    public Cadastro buscarPorId(Long id) {
+        return em.find(Cadastro.class, id);
+    }
+
+    public void cadastrar(Cadastro cadastro){
+        this.em.persist(cadastro);
+    }
+
+    public void atualizar(Cadastro cadastro) {
+        this.em.merge(cadastro);
     }
 
 }
