0e1c31c64ceac23d5cbdf95e0ad50a9f20f7b88d
g-andradd <gabrielandraad@gmail.com>
2022-04-20 09:20:32 -0300

    crud ClienteDAO feito

--- src/main/java/br/com/alura/srtch/dao/ClienteDAO.java
+++ src/main/java/br/com/alura/srtch/dao/ClienteDAO.java
@@ -3,6 +3,7 @@ package br.com.alura.srtch.dao;
 import br.com.alura.srtch.modelo.Cliente;
 
 import javax.persistence.EntityManager;
+import java.util.List;
 
 public class ClienteDAO {
 
@@ -12,8 +13,26 @@ public class ClienteDAO {
         this.em = em;
     }
 
+    public List<Cliente> buscarTodos() {
+        String jpql = "SELECT c FROM Cliente c";
+        return em.createQuery(jpql, Cliente.class).getResultList();
+    }
+
+    public Cliente buscarPorId(String cpf) {
+        return em.find(Cliente.class, cpf);
+    }
+
     public void cadastrar(Cliente cliente){
         this.em.persist(cliente);
+    }
+
+    public void atualizar(Cliente cliente) {
+        this.em.merge(cliente);
+    }
+
+    public void remover(Cliente cliente) {
+        cliente = em.merge(cliente);
+        this.em.remove(cliente);
     }
 
 }
