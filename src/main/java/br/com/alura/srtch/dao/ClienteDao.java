7ff1920434b3cb1c1c969507e93ffd6709fdbe28
jonatancampos-sortech <jonatan.campos@sortech.com.br>
2022-04-22 09:37:13 -0300

    JPA, DAO, PERSISTENCE.XML

---
+++ src/main/java/br/com/alura/srtch/dao/ClienteDao.java
@@ -0,0 +1,55 @@
+package br.com.alura.srtch.dao;
+
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+
+import br.com.alura.srtch.*;
+
+public class ClienteDao {
+	private EntityManager em;
+
+	public ClienteDao(EntityManager em) {
+		super();
+		this.em = em;
+	}
+	
+	public void cadastrar(Cliente cliente) {
+		this.em.persist(cliente);
+	}
+	
+	public void atualizar(Cliente cliente) {
+		this.em.merge(cliente);
+	}
+	
+	public void remover(Cliente cliente) {
+		cliente = em.merge(cliente); // só remove se o objeto estiver managed
+		this.em.remove(cliente);
+	}
+	
+	public Cliente buscarPorId(Long id) {
+		return em.find(Cliente.class, id);
+	}
+	
+	public List<Cliente> buscarTodos() {	
+		String jpql = "SELECT c FROM Cliente c";
+		return em.createQuery(jpql, Cliente.class).getResultList();
+	}
+	
+	public List<Cliente> buscarPorNome(String nome) {	
+		String jpql = "SELECT c FROM Cliente c WHERE c.nome = :nome"; // também funciona de :nome para ?1 indicando a posição 1
+		return em.createQuery(jpql, Cliente.class).setParameter("nome",nome).getResultList(); // também funciona de "nome" para 1, indicando a posição 1
+	}
+	
+	public List<Cliente> buscarPorAtivo() {	
+		String jpql = "SELECT c FROM Cliente c WHERE c.status = 'Ativo'"; 
+		return em.createQuery(jpql, Cliente.class).getResultList();
+	}
+	
+	public List<Cliente> buscarPorSuspenso() {	
+		String jpql = "SELECT c FROM Cliente c WHERE c.status = 'Suspenso'"; 
+		return em.createQuery(jpql, Cliente.class).getResultList();
+	}
+	
+}
