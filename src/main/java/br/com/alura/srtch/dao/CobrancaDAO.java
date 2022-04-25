d0abd724962262c794608d194fab3d8f1efffd98
Gabriel Andrade Almeida <gabrielandraad@gmail.com>
2022-04-25 11:31:01 -0300

    CobrancaDAO basica feita

--- src/main/java/br/com/alura/srtch/dao/CobrancaDAO.java
+++ src/main/java/br/com/alura/srtch/dao/CobrancaDAO.java
@@ -1,4 +1,38 @@
 package br.com.alura.srtch.dao;
 
+import br.com.alura.srtch.modelo.Cobranca;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
 public class CobrancaDAO {
+
+    private EntityManager em;
+
+    public CobrancaDAO(EntityManager em) {
+        this.em = em;
+    }
+
+    public List<Cobranca> buscarTodos() {
+        String jpql = "SELECT c FROM Cobranca c";
+        return em.createQuery(jpql, Cobranca.class).getResultList();
+    }
+
+    public Cobranca buscarPorId(long id) {
+        return em.find(Cobranca.class, id);
+    }
+
+    public void cadastrar(Cobranca cobranca){
+        this.em.persist(cobranca);
+    }
+
+    public void atualizar(Cobranca cobranca) {
+        this.em.merge(cobranca);
+    }
+
+    public void remover(Cobranca cobranca) {
+        cobranca = em.merge(cobranca);
+        this.em.remove(cobranca);
+    }
+
 }
