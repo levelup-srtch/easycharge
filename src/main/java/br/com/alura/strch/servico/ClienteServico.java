f5f04115d80c00ff759a42474a008976f3d2a957
cardoso21 <wagner.cardoso20@gmail.com>
2022-05-03 15:22:40 -0300

    Atualizando ClinenteServico

--- src/main/java/br/com/alura/strch/servico/ClienteServico.java
+++ src/main/java/br/com/alura/strch/servico/ClienteServico.java
@@ -13,7 +13,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.io.Serializable;
-import java.util.ArrayList;
 import java.util.List;
 
 @RequiredArgsConstructor
--- src/main/java/br/com/alura/strch/servico/DividaServico.java
+++ src/main/java/br/com/alura/strch/servico/DividaServico.java
@@ -3,7 +3,9 @@ package br.com.alura.strch.servico;
 import br.com.alura.strch.dominio.Divida;
 import br.com.alura.strch.repositorio.DividaRepositorio;
 import br.com.alura.strch.servico.DTO.DividaDTO;
+import br.com.alura.strch.servico.DTO.SelectDTO;
 import br.com.alura.strch.servico.excecao.ObjectnotFoundException;
+import br.com.alura.strch.servico.filtro.DividaFiltro;
 import br.com.alura.strch.servico.mapper.DividaMapper;
 import br.com.alura.strch.servico.mapper.DividaSetcMapper;
 import lombok.RequiredArgsConstructor;
@@ -11,6 +13,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.io.Serializable;
+import java.util.List;
 
 @RequiredArgsConstructor
 @Transactional
@@ -22,10 +25,42 @@ public class DividaServico implements Serializable {
     private final DividaSetcMapper dividaSetcMapper;
 
     public DividaDTO encontrarPorId(Long id){
-        Divida divida = dividaRepositorio.findById(id).orElseThrow(ObjectnotFoundException::new);
+        Divida divida = dividaRepositorio.findById(id).orElseThrow(()-> new ObjectnotFoundException("divida não encontrada!" + id));
         return dividaMapper.toDTO(divida);
     }
 
+    public List<DividaDTO> buscarTodas(){
+        return dividaMapper.toDTO(dividaRepositorio.OrderByDate());
+    }
+
+    public boolean validadorDivida (DividaDTO dividaDTO){
+        if(!dividaRepositorio.existsByDataAbertura(dividaDTO.getDataAbertura())){
+            return true;
+        }
+        throw new ObjectnotFoundException("Essa divida já esta em ABERTO!!" + dividaDTO.getDescricao());
+    }
 
 
+    public DividaDTO salvar (DividaDTO dividaDTO){
+        if(validadorDivida(dividaDTO)){
+            Divida divida = dividaMapper.toEntity(dividaDTO);
+            Divida dividaSalvar = dividaRepositorio.save(divida);
+            return dividaMapper.toDTO(dividaSalvar);
+        }
+        throw new ObjectnotFoundException("divida esta em Aberto!!" + dividaDTO.getDescricao());
+    }
+
+    public DividaDTO editar(DividaDTO dividaDTO){
+        Divida divida = dividaMapper.toEntity(dividaDTO);
+        Divida dividaAtualizar = dividaRepositorio.save(divida);
+        return dividaMapper.toDTO(dividaAtualizar);
+    }
+
+    public void excluirDivida(Long id){
+        dividaRepositorio.deleteById(id);
+    }
+
+    public List<DividaDTO> buscarPorFiltros(DividaFiltro dividaFiltro){
+        return dividaMapper.toDTO(dividaRepositorio.findAll(dividaFiltro.filter()));
+    }
 }
