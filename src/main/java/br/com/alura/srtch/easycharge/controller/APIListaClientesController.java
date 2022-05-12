eda4a409cf15eefc44969a2466457c5b3792af14
jonatancampos-sortech <jonatan.campos@sortech.com.br>
2022-05-12 14:55:36 -0300

    API LISTA CLIENTES SOMENTE VALORES DO CARD

--- src/main/java/br/com/alura/srtch/easycharge/controller/APIListaClientesController.java
+++ src/main/java/br/com/alura/srtch/easycharge/controller/APIListaClientesController.java
@@ -1,6 +1,5 @@
 package br.com.alura.srtch.easycharge.controller;
 
-import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +9,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import br.com.alura.srtch.easycharge.dto.ClienteDTO;
 import br.com.alura.srtch.easycharge.modelo.Cliente;
 import br.com.alura.srtch.easycharge.repository.ClienteRepository;
 
@@ -19,6 +19,7 @@ public class APIListaClientesController {
 	@Autowired
 	private ClienteRepository clienterepository;
 	
+	/*
 	@RequestMapping("/api/listaclientes")
 	@ResponseBody
 	public List<Cliente> home(Model model) { 
@@ -27,6 +28,15 @@ public class APIListaClientesController {
 		model.addAttribute("clientes", clientes);
 		// return Arrays.asList(clientes,clientes,clientes); // para retornar um array com varias listas de clientes
 		   return (clientes);
+	}
+	*/
+	
+	@RequestMapping("/api/listaclientes")
+	@ResponseBody
+	public List<ClienteDTO> home2(Model model) { 
+		List<Cliente> clientes = clienterepository.findAll(Sort.by(Sort.Direction.ASC, "nome"));
+		model.addAttribute("clientes", clientes);
+		   return ClienteDTO.convert(clientes);
 	}
 	
 	
---
+++ src/main/java/br/com/alura/srtch/easycharge/dto/ClienteDTO.java
@@ -0,0 +1,111 @@
+package br.com.alura.srtch.easycharge.dto;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import br.com.alura.srtch.easycharge.modelo.Cliente;
+import br.com.alura.srtch.easycharge.modelo.StatusCliente;
+
+public class ClienteDTO {
+
+	   private Long id;
+	 
+	   private String nome;
+	   
+	   private String cpf;
+	  
+	   private String telefone;
+	  
+	   private String local;
+	 
+	   private BigDecimal renda;
+	
+	   private StatusCliente  status;
+	   
+/*
+	public ClienteDTO(Long id, String nome, String cpf, String telefone, String local, BigDecimal renda,
+			StatusCliente status) {
+		this.id = id;
+		this.nome = nome;
+		this.cpf = cpf;
+		this.telefone = telefone;
+		this.local = local;
+		this.renda = renda;
+		this.status = status;
+	} 
+	
+	*/
+	public ClienteDTO(Cliente cliente) {
+		this.id = cliente.getId();
+		this.nome = cliente.getNome();
+		this.cpf = cliente.getCpf();
+		this.telefone = cliente.getTelefone();
+		this.local =  cliente.getEndereco().getCidade() + "/" +  cliente.getEndereco().getEstado();
+		this.renda = cliente.getRenda();
+		this.status = cliente.getStatus();
+	}
+
+
+	@Override
+	public String toString() {
+		return "ClienteDTO [nome=" + nome + ", cpf=" + cpf + ", telefone=" + telefone + ", local=" + local + ", renda="
+				+ renda + ", status=" + status + "]";
+	}
+
+
+	public Long getId() {
+		return id;
+	}
+
+
+
+
+	public String getNome() {
+		return nome;
+	}
+
+
+
+
+	public String getCpf() {
+		return cpf;
+	}
+
+
+	
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+
+	
+
+	public String getLocal() {
+		return local;
+	}
+
+
+
+	public BigDecimal getRenda() {
+		return renda;
+	}
+
+
+	
+
+	public StatusCliente getStatus() {
+		return status;
+	}
+
+
+	public static List<ClienteDTO> convert(List<Cliente> clientes) {
+		return clientes.stream().map(ClienteDTO::new).collect(Collectors.toList());
+	}
+	
+	   
+	   
+}
+
+
