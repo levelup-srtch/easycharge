51a64da414df51a8b183a2cd0033f9eac3a07175
Gabriel Andrade Almeida <gabrielandraad@gmail.com>
2022-05-16 11:13:01 -0300

    Card do Cache de clientes/report feito

--- src/main/java/br/com/alura/srtch/controller/ClientesRestController.java
+++ src/main/java/br/com/alura/srtch/controller/ClientesRestController.java
@@ -8,11 +8,15 @@ import br.com.alura.srtch.model.Cliente;
 import br.com.alura.srtch.projection.RelatorioClientesProjecao;
 import br.com.alura.srtch.repository.ClienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.web.JsonPath;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
@@ -21,24 +25,22 @@ import java.net.URI;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/clientes")
+@RequestMapping
 public class ClientesRestControler {
 
     @Autowired
     private ClienteRepository clienteRepository;
 
-    @GetMapping
-    public Page<ClientesResponseDto> lista(Integer page){
-        if(page == null){
-            page = 0;
-        }
-        Pageable pageable = PageRequest.of(page, 5, Sort.by(Sort.Direction.ASC, "status")
-                .and(Sort.by(Sort.Direction.ASC, "dadosPessoais.nome")));
-        return clienteRepository.findAll(pageable).map(ClientesResponseDto::new);
+    @GetMapping("/api/clientes")
+    public Page<ClientesResponseDto> lista(@PageableDefault(size = 5, sort = {"dadosPessoais.nome", "status"},
+            direction = Direction.ASC) Pageable pageable) {
+        Page<Cliente> clientes = clienteRepository.findAll(pageable);
+        return ClientesResponseDto.converter(clientes);
     }
 
-    @PostMapping
-    public ResponseEntity<ClienteDto> cadastrar(@RequestBody @Valid ClienteForm form, UriComponentsBuilder uriBuilder){
+    @PostMapping("/api/clientes")
+    @Transactional
+    public ResponseEntity<ClienteDto> cadastrar(@RequestBody @Valid ClienteForm form, UriComponentsBuilder uriBuilder) {
         Cliente cliente = new ClienteMapper().cadastrar(form);
         clienteRepository.save(cliente);
 
@@ -46,9 +48,11 @@ public class ClientesRestControler {
         return ResponseEntity.created(uri).body(new ClienteDto(cliente));
     }
 
-    @GetMapping("/report")
-    public List<RelatorioClientesProjecao> listarReport(){
+    @GetMapping("/api/clientes/report")
+    @Cacheable(value = "listaDeClientes")
+    public List<RelatorioClientesProjecao> listarReport() {
         return clienteRepository.findNomeValorDasDividasCobrancas();
     }
+
 
 }
