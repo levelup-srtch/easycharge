6b6d3d0c666cd036cee8634b4edf5c17083a6bed
Cardoso21 <wagnerjcardoso21@gmail.com>
2022-05-23 11:45:36 -0300

    Revert "Revert "Revert "criando cobrancaRepositori"""
    This reverts commit a368738287c568420329568771a32a299825d15c.

--- src/main/java/br/com/alura/strch/repositorio/CobrancaRepositori.java
+++ src/main/java/br/com/alura/strch/repositorio/CobrancaRepositori.java
@@ -1,16 +1,10 @@
 package br.com.alura.strch.repositorio;
 
-import br.com.alura.strch.dominio.Cliente;
 import br.com.alura.strch.dominio.Cobranca;
-import br.com.alura.strch.dominio.Divida;
 import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
 
 public interface CobrancaRepositori extends JpaRepository <Cobranca, Long> {
 
     boolean existsById(long id);
-    boolean existsByDivida(Divida divida);
-    List<Cobranca> getAllByDivida(Divida divida);
 
 }
