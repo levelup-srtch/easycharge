197b2a5d88dc8d5aa78411b0dd18957b76889a65
Gabriel Andrade Almeida <gabrielandraad@gmail.com>
2022-05-25 17:29:24 -0300

    formatação de cpf funcionando

--- src/main/resources/templates/js/formulario.js
+++ src/main/resources/templates/js/formulario.js
@@ -1,11 +1,25 @@
-function formataCPF(input) {
-    const cpfFormatado = input.value.replace(/\D/g, '');
+export function valida(input) {
+  const tipoDeInput = input.dataset.tipo
 
-    let cpfAtualizado;
-
-    cpfAtualizado = cpfAtual.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/,
-      function (regex, argumento1, argumento2, argumento3, argumento4) {
-        return argumento1 + '.' + argumento2 + '.' + argumento3 + '-' + argumento4;
-      })
-    elementoAlvo.value = cpfAtualizado;
+  if (validadores[tipoDeInput]) {
+    validadores[tipoDeInput](input);
   }
+}
+
+const validadores = {
+  cpf: input => formataCPF(input)
+}
+
+function formataCPF(cpf) {
+  document.getElementById("cpf").addEventListener("keydown", formataCPF);
+  const elementoAlvo = cpf
+  const cpfAtual = cpf.value
+
+  let cpfAtualizado;
+
+  cpfAtualizado = cpfAtual.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/,
+    function (regex, argumento1, argumento2, argumento3, argumento4) {
+      return argumento1 + '.' + argumento2 + '.' + argumento3 + '-' + argumento4;
+    })
+  elementoAlvo.value = cpfAtualizado;
+}
